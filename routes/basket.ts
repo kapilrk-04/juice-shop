22e2f2c44a91674174362eb0f495946b14651813
kapilrk-04 <kapil.rajesh@students.iiit.ac.in>
2024-09-15 23:24:48 +0530

    rectified some changes

--- routes/basket.ts
+++ routes/basket.ts
@@ -15,7 +15,8 @@ const security = require('../lib/insecurity')
 module.exports = function retrieveBasket () {
   return (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id
-    BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
+    const user = security.authenticatedUsers.from(req)
+    BasketModel.findOne({ where: { bid: user.bid }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
       .then((basket: BasketModel | null) => {
         /* jshint eqeqeq:false */
         challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -12,10 +12,26 @@ import * as utils from '../lib/utils'
 const security = require('../lib/insecurity')
 const request = require('request')
 
+const validDomains = ['imgur.com', 'i.imgur.com'];
+
+function isValidUrl(url: string) {
+  try {
+    const parsedUrl = new URL(url);
+    return validDomains.includes(parsedUrl.hostname); 
+  } catch (err) {
+    return false; 
+  }
+}
+
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
+      
+      if (!isValidUrl(url)) {
+        return res.status(400).send('Invalid URL');
+      }
+
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
@@ -28,7 +44,8 @@ module.exports = function profileImageUrlUpload () {
           .on('response', function (res: Response) {
             if (res.statusCode === 200) {
               const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
-              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
+              const sanitizedUserId = String(loggedInUser.data.id).replace(/[^a-zA-Z0-9]/g, '');
+              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${sanitizedUserId}.${ext}`))
               UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
             } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
           })
